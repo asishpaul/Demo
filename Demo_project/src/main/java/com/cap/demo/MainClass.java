bb508b67f7a6bdb4ed86b560417dcc17b33a2ba9
aspaul <aspaul@DIN73003064.corp.capgemini.com>
2018-11-23 14:32:14 +0530

    Demo Commit

---
+++ Demo_project/src/main/java/com/cap/demo/MainClass.java
@@ -0,0 +1,15 @@
+package com.cap.demo;
+
+public class MainClass {
+
+	void display() {
+		System.out.println("This is Main");
+	}
+	public static void main(String[] args) {
+		
+		MainClass mc=new MainClass();
+		mc.display();
+
+	}
+
+}
